39c32c6831768a05b7eaebc23e344e565f483c0f
Ly Leo <thienly.leo@gmail.com>
2023-04-02 13:30:47 +0700

    Update tempalte for Trung Hieu Kim Hieu

--- templates/lovely-wedding-03/js/customizer.js
+++
@@ -1,7 +0,0 @@
- // prettyPhoto
-  $("a[rel^='prettyPhoto[gallery1]']").prettyPhoto(); 
-  $("a[rel^='prettyPhoto[iframes]']").prettyPhoto(); 
-  $("a[rel^='prettyPhoto']").prettyPhoto(); 
-
- 
-  
--- templates/our-love-responsive-wedding-template/preview/lmpixels-demo-panel.js
+++
@@ -1,1 +0,0 @@
-function styleSwitcher(){var demoPanel=$("#lm_demo_panel"),demoPanelSwitcher=$("#lm_demo_panel_switcher");$(window).on("click",function(){$("#lm_demo_panel.active").removeClass("active");});demoPanelSwitcher.on("click",function(event){event.stopPropagation();demoPanel.toggleClass("active");});demoPanel.on("click",function(event){event.stopPropagation();});};
